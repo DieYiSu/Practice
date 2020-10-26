de705e1ebdfb3741a7c70de395a6817d56f3114a
David Su <adshfly@gmail.com>
2020-10-27 01:41:38 +0800

    night code

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,18 @@
+// ESLint+prettier: Airbnb-coding-style
+
 const generateResultButton = document.querySelector('.generate-result-button');
-const heightInputValue = document.querySelector('#height-input');
-const weightInputValue = document.querySelector('#weight-input');
 
 function generateResultButtonClick() {
-  console.log('test');
-  console.log(heightInputValue.value);
-  console.log(weightInputValue.value);
+  const heightInputValue = document.querySelector('#height-input').value;
+  const weightInputValue = document.querySelector('#weight-input').value;
+
+  const bmiObject = {
+    datetime: new Date().toLocaleString(),
+    BMIresult:
+      weightInputValue / (((heightInputValue / 100) * heightInputValue) / 100),
+  };
+
+  console.log(bmiObject);
 }
 
 generateResultButton.addEventListener(
