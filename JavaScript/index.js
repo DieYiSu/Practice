664c389262db4772b4678d7727da4c3e75e62374
David Su <adshfly@gmail.com>
2020-10-27 18:42:10 +0800

    Complete button reset function
    1. the name reset-button has some problem when the vanilla.js use querySelector, it will return null, change the reset-button name to the resetButton, then it work, no idea what it going on.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,16 @@
 // ESLint+prettier: Airbnb-coding-style
 
 // todo
-// view - button change
 // view - result card
 // value - input value check
 
 const generateResultButton = document.querySelector('.generate-result-button');
+const buttonResult = document.querySelector('.result');
+const headerResultDescription = document.querySelector(
+  '.headerResultDescription'
+);
+console.log(headerResultDescription);
+const buttonResetButton = document.querySelector('.resetButton');
 
 function setLocalStorage(bmiObject) {
   const getLocalStorage = JSON.parse(localStorage.getItem('bmiObject')) || [];
@@ -35,6 +40,28 @@ function BmiDetermination(BMIresultValue) {
   }
 }
 
+function createCard(bmiObject) {
+  console.log(bmiObject);
+  const ulResult = document.querySelector('.main-content ul.result');
+
+  const ulResultString = ``;
+  switch (bmiObject.determinationDescription) {
+    case '理想':
+      break;
+    case '過輕':
+      break;
+    case '過重':
+      break;
+    case '輕度肥胖':
+      break;
+    case '中度肥胖':
+      break;
+    case '重度肥胖':
+      break;
+    // no default
+  }
+}
+
 function generateResultButtonClick() {
   const heightInputValue = document.querySelector('#height-input').value;
   const weightInputValue = document.querySelector('#weight-input').value;
@@ -52,7 +79,19 @@ function generateResultButtonClick() {
     determinationDescription: BmiDeterminationDescription,
   };
 
-  setLocalStorage(bmiObject);
+  generateResultButton.classList.add('displayNone');
+  buttonResult.classList.remove('displayNone');
+  buttonResetButton.classList.remove('displayNone');
+  buttonResetButton.classList.add('flex');
+  headerResultDescription.classList.remove('displayNone');
+}
+
+function resetButton() {
+  generateResultButton.classList.remove('displayNone');
+  buttonResult.classList.add('displayNone');
+  buttonResetButton.classList.add('displayNone');
+  buttonResetButton.classList.remove('flex');
+  headerResultDescription.classList.add('displayNone');
 }
 
 generateResultButton.addEventListener(
@@ -60,3 +99,5 @@ generateResultButton.addEventListener(
   generateResultButtonClick,
   false
 );
+
+buttonResetButton.addEventListener('click', resetButton, false);
