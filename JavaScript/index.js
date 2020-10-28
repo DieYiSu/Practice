9cc467899f9558bbc80f6dbf98d30b8e3a9de5d4
David Su <adshfly@gmail.com>
2020-10-28 21:58:38 +0800

    find the way to adjust the color

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,16 +4,19 @@
 // view - result card
 // value - input value check
 
-const generateResultButton = document.querySelector('.generate-result-button');
-const buttonResult = document.querySelector('.result');
+const generateResultButton = document.querySelector('.generateResultButton');
+const buttonResult = document.querySelector('.resultCircle');
 const headerResultDescription = document.querySelector(
   '.headerResultDescription'
 );
-console.log(headerResultDescription);
+console.log(headerResultDescription.classList);
 const buttonResetButton = document.querySelector('.resetButton');
+const getLocalStorage = JSON.parse(localStorage.getItem('bmiObject')) || [];
+const ulResult = document.querySelector('.main-content ul.result');
+const resultCircleValue = document.querySelector('.resultCircleValue');
+const resultCircle = document.querySelector('.resultCircle');
 
 function setLocalStorage(bmiObject) {
-  const getLocalStorage = JSON.parse(localStorage.getItem('bmiObject')) || [];
   getLocalStorage.push(bmiObject);
   localStorage.setItem('bmiObject', JSON.stringify(getLocalStorage));
 }
@@ -40,23 +43,188 @@ function BmiDetermination(BMIresultValue) {
   }
 }
 
-function createCard(bmiObject) {
-  console.log(bmiObject);
-  const ulResult = document.querySelector('.main-content ul.result');
+function createEmyptAlert() {
+  console.log('test');
+  const ulResultString = `
+    <h3 class="emptyAlert">你還沒有任何記錄！</h3>
+    <h3 class="emptyAlert">快去測量BMI！</h3>
+  `;
+  ulResult.innerHTML = ulResultString;
+}
 
-  const ulResultString = ``;
-  switch (bmiObject.determinationDescription) {
+function createCard() {
+  let ulResultString = ``;
+  Object.keys(getLocalStorage).forEach((index) => {
+    switch (getLocalStorage[index].determinationDescription) {
+      case '理想':
+        ulResultString += `
+          <li class="result-card result-card-Normal">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      case '過輕':
+        ulResultString += `
+          <li class="result-card result-card-Underweight">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      case '過重':
+        ulResultString += `
+          <li class="result-card result-card-Overweight">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      case '輕度肥胖':
+        ulResultString += `
+          <li class="result-card result-card-Moderately-obese">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      case '中度肥胖':
+        ulResultString += `
+          <li class="result-card result-card-Severely-obese">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      case '重度肥胖':
+        ulResultString += `
+          <li class="result-card result-card-Very-severely-obese">
+            <h3>${getLocalStorage[index].determinationDescription}</h3>
+            <ul class="result-card-param">
+              <li>
+                <p class="result-card-param-description">BMI</p>
+                <p class="result-card-param-value">${getLocalStorage[index].BMIresult}</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">weight</p>
+                <p class="result-card-param-value">${getLocalStorage[index].weight}kg</p>
+              </li>
+              <li>
+                <p class="result-card-param-description">height</p>
+                <p class="result-card-param-value">${getLocalStorage[index].height}cm</p>
+              </li>
+            </ul>
+            <p class="date">${getLocalStorage[index].datetime}</p>
+          </li>
+        `;
+        break;
+      // no default
+    }
+  });
+  ulResult.innerHTML = ulResultString;
+}
+
+function headerCircleSetting(BmiDeterminationDescription) {
+  switch (BmiDeterminationDescription) {
     case '理想':
+      headerResultDescription.innerHTML = `理想`;
+      // eslint-disable-next-line no-undef
+      // eslint-disable-next-line dot-notation
+      headerResultDescription.style['color'] = '#86D73F';
+      console.log(buttonResetButton);
+
       break;
     case '過輕':
+      headerResultDescription.innerHTML = `過輕`;
+      headerResultDescription.style.color = '#86D73F';
       break;
     case '過重':
+      headerResultDescription.innerHTML = `過重`;
+      headerResultDescription.style.color = '#86D73F';
       break;
     case '輕度肥胖':
+      headerResultDescription.innerHTML = `輕度肥胖`;
+      headerResultDescription.style.color = '#86D73F';
       break;
     case '中度肥胖':
+      headerResultDescription.innerHTML = `中度肥胖`;
+      headerResultDescription.style.color = '#86D73F';
       break;
     case '重度肥胖':
+      headerResultDescription.innerHTML = `重度肥胖`;
+      // console.log(headerResultDescription.style);
+      headerResultDescription.style.color = '#86D73F';
+      // console.log(buttonResetButton.style.background);
       break;
     // no default
   }
@@ -79,11 +247,20 @@ function generateResultButtonClick() {
     determinationDescription: BmiDeterminationDescription,
   };
 
+  // eslint-disable-next-line no-unused-expressions
+  if (BMIresultValue === 'NaN') {
+    window.alert(`Input has to be the number type`);
+    return;
+  }
+  resultCircleValue.innerHTML = `${BMIresultValue}`;
   generateResultButton.classList.add('displayNone');
   buttonResult.classList.remove('displayNone');
   buttonResetButton.classList.remove('displayNone');
   buttonResetButton.classList.add('flex');
   headerResultDescription.classList.remove('displayNone');
+  setLocalStorage(bmiObject);
+  headerCircleSetting(BmiDeterminationDescription);
+  createCard();
 }
 
 function resetButton() {
@@ -93,6 +270,15 @@ function resetButton() {
   buttonResetButton.classList.remove('flex');
   headerResultDescription.classList.add('displayNone');
 }
+
+window.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    // eslint-disable-next-line no-unused-expressions
+    getLocalStorage.length === 0 ? createEmyptAlert() : createCard();
+  },
+  false
+);
 
 generateResultButton.addEventListener(
   'click',
