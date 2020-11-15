35367cc8cdc1a6219ed8c81948b9e916f3771163
David Su <adshfly@gmail.com>
2020-11-15 23:41:20 +0800

    DS: BST Completed
    DS: BST Completed

--- DSA/Data-Structure/Tree/BinarySearchTree.js
+++ DSA/Data-Structure/Tree/BinarySearchTree.js
@@ -2,10 +2,12 @@
  * refer youtube video: 
  * - https://www.youtube.com/watch?v=5cU1ILGy6dM
  * - https://www.youtube.com/watch?v=wcIRPqTR3Kc&list=PLLXdhg_r2hKA7DPDsunoDZ-Z769jWn4R8&index=9&t=0s&ab_channel=mycodeschool
+ * refer code:
+ * - https://codepen.io/DieYiSu/pen/XWKOLQe?editors=0011
  */
 
 class Node {
-    constructor(data, left = null, right = null){
+    constructor(data, left = null, right = null) {
         this.data = data;
         this.left = left;
         this.right = right;
@@ -13,31 +15,31 @@ class Node {
 }
 
 class BST {
-    constructor(){
+    constructor() {
         this.root = null;
     }
 
-    add(data){
+    add(data) {
         const node = this.root;
-        if(node === null){
+        if (node === null) {
             this.root = new Node(data);
-        }else { //if there's already has a root node
-            const searchTree = function(node){
-                if(data < node.data){ //if the add data is smaller than parent node.data, go to the left-tree
-                    if(node.left === null){
+        } else { //if there's already has a root node
+            const searchTree = function (node) {
+                if (data < node.data) { //if the add data is smaller than parent node.data, go to the left-tree
+                    if (node.left === null) {
                         node.left = new Node(data);
                         return;
-                    }else if (node.left !== null){
+                    } else if (node.left !== null) {
                         return searchTree(node.left)
                     }
-                }else if (data > node.data){
-                    if(node.right === null){
+                } else if (data > node.data) {
+                    if (node.right === null) {
                         node.right = new Node(data);
                         return;
-                    }else if (node.right !== null){
+                    } else if (node.right !== null) {
                         return searchTree(node.right);
                     }
-                }else {
+                } else {
                     return null;
                 }
             }
@@ -45,34 +47,34 @@ class BST {
         }
     }
 
-    findMin(){
+    findMin() {
         let current = this.root;
-        while(current.left !== null){
+        while (current.left !== null) {
             current = current.left;
         };
         console.log(current.data);
         return current.data;
     }
 
-    findMax(){
+    findMax() {
         let current = this.root;
-        while (current.right !== null){
+        while (current.right !== null) {
             current = current.right;
         }
         console.log(current.data);
         return current.data
     }
 
-    isPresent(data){
+    isPresent(data) {
         let current = this.root;
-        while(current){
-            if(current.data === data){
+        while (current) {
+            if (current.data === data) {
                 console.log(`True`);
                 return true;
             }
-            if(data < current.data){
+            if (data < current.data) {
                 current = current.left;
-            }else {
+            } else {
                 current = current.right;
             }
         }
@@ -80,25 +82,38 @@ class BST {
         return false;
     }
 
-    remove(data){
-        const removeNode = function(node, data){
-            if(node === null){ // empty tree?
+    remove(data) {
+        const removeNode = function (node, data) {
+            if (node == null) { // empty tree?
                 return null;
             }
-            if(data == node.data){
+            if (data == node.data) {
                 // node has no child
-                if(node.left == null && node.right == null){
+                if (node.left == null && node.right == null) {
                     return null;
                 }
                 // node has no left child, then replace it by the right node
-                if(node.left == null){
+                if (node.left == null) {
                     return node.right;
                 }
-                if(node.right == null){
+                if (node.right == null) {
                     return node.left;
                 }
                 // node has both left and right child
                 // I get confusing about the delete pseudocode.
+                var tempNode = node.right;
+                while (tempNode.left !== null) {
+                    tempNode = tempNode.left;
+                }
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            } else if (data < node.data) {
+                node.left = removeNode(node.left, data)
+                return node;
+            } else {
+                node.right = removeNode(node.right, data)
+                return node;
             }
         }
         this.root = removeNode(this.root, data);
@@ -107,17 +122,24 @@ class BST {
 
 const bst = new BST();
 
-bst.add(9);
-bst.add(4);
-bst.add(17);
+bst.add(1);
+bst.add(2);
 bst.add(3);
+// bst.add(3);
 // bst.add(6);
 // bst.add(22);
 // bst.add(5);
 // bst.add(7);
 // bst.add(20);
 
+// bst.findMax()
+// bst.findMin()
+bst.remove(1)
+bst.remove(3)
+bst.findMin()
+// bst.remove(3)
 bst.findMax()
+// bst.remove(3)
 
 
-// console.log(bst)
+console.log(bst)
