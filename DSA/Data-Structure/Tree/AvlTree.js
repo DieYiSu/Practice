9cba939d778c9096e8e1b70fd3adf5bc9f3c346a
David Su <adshfly@gmail.com>
2021-03-22 23:09:46 +0800

    Add Data Structure: AVL Tree

---
+++ DSA/Data-Structure/Tree/AvlTree.js
@@ -0,0 +1,16 @@
+/**
+ * refer youtube video: 
+ * - https://www.youtube.com/watch?v=FNeL18KsWPc&ab_channel=MITOpenCourseWare
+ * - https://www.youtube.com/watch?v=vRwi_UcZGjU&ab_channel=BackToBackSWE
+ * refer:
+ * - https://josephjsf2.github.io/data/structure/and/algorithm/2019/06/22/avl-tree.html
+ */
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
